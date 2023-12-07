1e33a500d95279f1f09c850928216e8ea5d057c9
Selvakumar D <selvasudar3@gmail.com>
2023-12-07 22:52:15 +0530

    first commit

---
+++ service.py
@@ -0,0 +1,28 @@
+# backend.py
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from pytube import Playlist
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/download_playlist', methods=['POST'])
+def download_playlist():
+    print("service works")
+    data = request.get_json()
+    playlist_url = data.get('playlist_url')
+    output_path = './public/downloads'
+
+    playlist = Playlist(playlist_url)
+    video_paths = []
+
+    for video in playlist.videos:
+        video_path = os.path.join(output_path, video.title + '.mp4')
+        video.streams.get_highest_resolution().download(output_path)
+        video_paths.append(video_path)
+    
+    return jsonify({'message': 'Download complete', 'video_paths': video_paths})
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// frontend/src/App.js
+import React, { useState } from 'react';
 
 function App() {
+  const [playlistUrl, setPlaylistUrl] = useState('');
+
+  const handleDownload = async (e) => {
+    e.preventDefault();
+    console.log('Handling download...');
+    try {
+      const response = await fetch('http://localhost:5000/download_playlist', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ playlist_url: playlistUrl }),
+      });
+
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error('Error downloading playlist:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='row text-center mt-5'>
+      <div className='col-12'>
+        <h1>YouTube Playlist Downloader</h1>
+        <h6>- Selva D</h6>
+        <input
+          type="text"
+          value={playlistUrl}
+          onChange={(e) => setPlaylistUrl(e.target.value)}
+          placeholder="Enter YouTube Playlist URL"
+          className='form-control w-25 mx-auto mt-5'
+        />
+        <button type="button" className='btn btn-primary mt-2' onClick={handleDownload}>Download Playlist</button>
+      </div>
     </div>
   );
 }
